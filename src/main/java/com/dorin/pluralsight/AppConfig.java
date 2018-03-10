cf6d3caa4eb5f7a69c659b111cffd234fc30504a
dorin <dorin.popa1995@gmail.com>
2018-03-11 01:09:21 +0200

    Add app.properties to application via AppConfig

--- src/main/java/com/dorin/pluralsight/AppConfig.java
+++ src/main/java/com/dorin/pluralsight/AppConfig.java
@@ -1,10 +1,18 @@
 package com.dorin.pluralsight;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.dorin.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
--- src/main/java/com/dorin/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/dorin/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.dorin.pluralsight.repository;
 
 import com.dorin.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +10,12 @@ import java.util.List;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
     @Override
     public List<Customer> findAll() {
+        System.out.println("dbUsername: " + dbUsername);
         List<Customer> customers = new ArrayList<>();
 
         Customer customer = new Customer();
