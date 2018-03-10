68465653e36676e50ca8facfcd2b37eb3ea9e299
dorin <dorin.popa1995@gmail.com>
2018-03-11 00:45:57 +0200

    Show if scope is default - singleton then you obtain same object on each request per spring container

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,6 +10,11 @@ public class Application {
 
         CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
 
+        System.out.println(service);
+
+        CustomerService service2 = applicationContext.getBean("customerService", CustomerService.class);
+        System.out.println(service2);
+
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/dorin/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/dorin/pluralsight/service/CustomerServiceImpl.java
@@ -3,11 +3,13 @@ package com.dorin.pluralsight.service;
 import com.dorin.pluralsight.model.Customer;
 import com.dorin.pluralsight.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
+@Scope("singleton")
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
